35ae1244d75ec65bdbdbaca8e403b393592c2098
Trevor Gituru <giturutrevor@gmail.com>
2025-07-01 18:04:03 +0300

    Added SEO

--- frontend-central/src/app/layout.tsx
+++ frontend-central/src/app/layout.tsx
@@ -28,6 +28,37 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
+        {/* JSON-LD Structured Data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "SoftwareApplication",
+              "name": "Solaris Conexus",
+              "operatingSystem": "Web",
+              "applicationCategory": "Energy Trading Platform",
+              "description":
+                "Solaris Conexus is a decentralized energy trading platform that uses IoT and blockchain to enable solar power producers and consumers to trade using PowerTokens.",
+              "url": "https://solaris.razaoul.xyz",
+              "image": "https://www.razaoul.xyz/preview.jpg",
+              "creator": {
+                "@type": "Person",
+                "name": "Trevor Muriuki Gituru",
+              },
+              "offers": {
+                "@type": "Offer",
+                "price": "0",
+                "priceCurrency": "USD",
+              },
+              "sameAs": [
+                "https://github.com/trevor-gituru/solaris_conexus.git",
+                "https://sepolia.voyager.online/contract/0x0696e1ba36b1b46ccfda4a6ae963b12c932e36d1a1008d5d0a03033b12d86827"
+              ],
+            }),
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
